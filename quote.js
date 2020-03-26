239ffac2d24714c8ef4624a5c8bc61b5034fa22d
Meghan Martin <meghanxmartin@gmail.com>
2020-03-26 17:15:28 +0100

    refactors code and finishes project

--- quote.js
+++ quote.js
@@ -1,14 +1,22 @@
 const quoteTag = document.querySelector("h1")
 const authorTag = document.querySelector("p")
 const randomTag = document.querySelector("footer img")
+const bodyTag = document.querySelector("body")
 
-let data = []
 const getQuote = () => {
-  if (data.length > 0 ) {
-    const randomQuote = data[Math.floor(data.length * Math.random())]
-    quoteTag.innerHTML = randomQuote.quote
-    authorTag.innerHTML = randomQuote.author
+fetch("https://api.superhi.com/api/test/quotes/random")
+  .then(response => response.json())
+  .then(jsonData =>  {
+    quoteTag.innerHTML = "&ldquo;" + jsonData.quote + "&rdquo;"
+    authorTag.innerHTML = "&mdash; " + jsonData.author
+  
+  if(jsonData.quote.length > 90 ) {
+    quoteTag.classList.add("long")
+  } else {
+    quoteTag.classList.remove("long")
   }
+    bodyTag.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 90%)`
+  })
 }
 
 getQuote()
